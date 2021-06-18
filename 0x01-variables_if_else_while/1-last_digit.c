9c4b98438bf8da2638954e301890279abb236a40
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-06-18 13:36:20 -0700

    low level programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ last = n % 10;
 
 if (last > 5)
 {
-  printf("Last digit of %d is %d and is greater than 5\n", n, last);
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
 else if (last < 6 && !last == 0)
 {
