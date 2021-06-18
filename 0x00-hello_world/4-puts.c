8cabcc5f50788a042e420fc6dbd3bc1cc57e6e72
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-06-18 08:18:18 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-    puts("\"programming is like building a multilingual puzzel");
-    return (0);
+        puts("\"programming is like building a multilingual puzzel");
+        return (0);
 }
