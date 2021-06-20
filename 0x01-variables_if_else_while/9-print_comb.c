e01002a2aa72c4327470b0d63ffc396fe73ba702
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-06-20 10:49:47 -0700

    low level programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int integer;
+for (integer = 0 ; integer <= 9 ; integer++)
+{
+putchar(integer + '0' );
+if (integer != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
