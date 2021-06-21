1031b655aedf85c4e54cb3bd907f11a69ddb48a7
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-06-21 07:52:51 -0700

    low level programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int  i;
+int j;
+for (i = 48 ; i < 58 ; i++)
+{
+for (j = i ; j < 58 ; j++)
+{
+if (i == j)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
