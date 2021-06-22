1f3aafe19a9691f9938314a41eb4264ef260955e
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-06-22 06:57:52 -0700

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*main - Entry point
+*/
+void print_alphabet(void)
+{
+char character;
+for (character = 'a' ; character <= 'z' ; chracter++)
+_putchar(character);
+_putchar('\n');
+}
