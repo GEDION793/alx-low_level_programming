bffb3677220b5cf94b38a9f6004d8b90cad56850
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-06-23 09:24:12 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
-*_islower - Checks if a character is lowercase.
-*@c: The character to be checked.
-*
-*Return: 1 if character is lowercase, 0 otherwise.
+* _islower - function checks for lowercase character
+* @c:  -is the variable used for in the program
+* Return: Always 0.
 */
 int _islower(int c)
 {
-if(c >= 'a' && c <= 'z')
-return (1);
+if (c >= 'a' && c <= 'z')   
+{
+return (1); 
+}
 else
 return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,25 @@
 #include "holberton.h"
 
 /**
-*print_sign - Prints the sign of a number.
-*@n: The number of which the sign will be printed.
-*
-*Return: 1 if the number is greater than zero,
-0 if the number is zero,
--1 if the number is less than zero.
+* print_sign - Function checks for sign of a number
+* @n: argument used in the function
+* Return: Always 0.
 */
 int print_sign(int n)
 {
-if (n > 0)
+if (n > 0)   
 {
-_putchar('+');
+_putchar('+'); 
 return (1);
 }
-else if (n == 0)
+else if (n < 0)
 {
-_putchar('0');
-return (0);
+_putchar('-'); 
+return (-1);
 }
 else
 {
-_putchar('-');
-return (-1);
+_putchar('0'); 
+return (0);
 }
 }
