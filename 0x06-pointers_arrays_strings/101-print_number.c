d0364f2c4b9a0ce3c66d480c38b65ec03fc0bd68
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-07-01 12:25:49 -0700

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
-
-int main(void)
-{
-int n;
-int a[5];
-int *p;
-
-a[2] = 1024;
-p = &n;
-
+#include "holberton.h"
 /**
-*main - check the code for Holberton School students.
-*/
-p[5] = 98;
-printf("a[2] = %d\n", a[2]);
+*exponent - x to the power of y
+*@x: base number
+*@y: exponent
+*Description: calcuates x^y
+*Return: x^y
+*
+**/
+int exponent(int x, int y)
+{
+int power;
+power = x;
+if (x == 0)
 return (0);
+if (y == 0)
+return (1);
+while (y >= 2)
+{
+power  = power *x;
+y--;
+}
+return (power);
+}
+/**
+*print_number - print an int using only _putchar
+*@number: int to be printed by function
+*
+*Return: nothing
+**/
+void print_number(int number)
+{
+int size, digit, counter, sign;
+sign = 1;
+digit = 0;
+size = 1;
+counter = number;
+if (number < 0)
+{
+_putchar('-');
+sign = -1;
+}
+for (; counter >= 10 || counter <= -10; size++)
+{
+counter = counter / 10;
+}
+counter = number;
+while (size >= 2)
+{
+digit = (counter / exponent(10, size - 1)) * sign;
+_putchar(digit + '0');
+counter = counter % exponent(10, size - 1);
+size--;
+}
+_putchar(sign *counter % 10 + '0');
 }
