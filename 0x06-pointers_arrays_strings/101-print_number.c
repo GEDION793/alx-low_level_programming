af537ba8fcf6a0dfa2300f460d182e28a85a4c50
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-07-01 11:59:46 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,18 @@
-#include "holberton.h"
+#include <stdio.h>
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
 
 /**
-*print_number - prints an interger.
-*@n: interger.
-*Return: nothing.
+*main - check the code for Holberton School students.
 */
-void print_number(int n)
-{
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-if (n / 10)
-print_number(n / 10);
-_putchar('0' + n % 10);
+p[5] = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
