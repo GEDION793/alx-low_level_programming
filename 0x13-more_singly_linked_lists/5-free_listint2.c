f7c412134fcca9362d1f12b8674f1718aa7c209a
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-08-04 13:18:35 -0700

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,24 @@
 #include "lists.h"
 
 /**
-*free_listint2 - function that frees a list
-*@head: head
-*Return: void
+*free_listint2 - frees a linked list
+*@head: head of a list.
+*
+*Return: no return.
 */
-
 void free_listint2(listint_t **head)
 {
-listint_t *tmp, *node;
+listint_t *temp;
+listint_t *curr;
 
-node = *head;
-
-while (node != NULL)
-
+if (head != NULL)
 {
-tmp = node;
-node = node->next;
-free(tmp);
+curr = *head;
+while ((temp = curr) != NULL)
+{
+curr = curr->next;
+free(temp);
 }
 *head = NULL;
+}
 }
