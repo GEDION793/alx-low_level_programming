d1c227f525559161e25401ef7274fa04978ecc46
GEDION793 <84683857+GEDION793@users.noreply.github.com>
2021-09-17 14:07:56 -0700

    doubly

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all elements of a doubly linked list
- * @h: pointer to head of list
- * Return: number of nodes
+ *print_dlistint - prints all elements of a doubly linked list
+ *@h: pointer to head of list
+ *Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h) 
 {
